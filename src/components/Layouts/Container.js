c255727be88d7ce2750cb0affa287ae436c74ccd
Charles Kasasira <charleskasasira01@gmail.com>
2023-01-12 03:35:41 +0300

    updated agent sidebar

--- src/components/Layouts/Container.js
+++ src/components/Layouts/Container.js
@@ -2,6 +2,7 @@ import useAuth from "contexts/Auth";
 import { useToggleMenu } from "hooks";
 import { SuperAdminMenu } from "pages";
 import { SupervisorMenu } from "pages";
+import { AdminMenu } from "pages";
 
 function Container({ children }) {
   const { currentUser, authClaims } = useAuth();
@@ -25,6 +26,7 @@ function Container({ children }) {
             maximiseMenu={maximiseMenu}
           />
         )}
+        {authClaims?.agent && <AgentMenus />}
       </div>
       <main>{children}</main>
     </div>
